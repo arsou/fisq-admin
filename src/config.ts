8522a33452b2e3b3c874f61a254dd22770e2b5f5
arsou <arsene31@gmail.com>
2021-08-13 02:40:50 -0400

    modification de config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,25 @@
-export const config={
+
+const is_dev=process.env.NODE_ENV?.startsWith('dev');
+const is_fisq=window.location.href.search(/festivalsalsaquebec/i)>=0;
+const site=is_dev?''
+            :(is_fisq?'festivalsalsaquebec.com':'quebecsensualweekend.com')
+
+            export const config={
   TIMEZONE:'America/Montreal',
-  SITE_CODE:'FISQ',
-  SITE_TITLE: 'FISQ:Festival International de Salsa de Quebec',
+  
+  SITE_CODE: is_dev || is_fisq? 'FISQ':'QSW',
+  
+  SITE_TITLE: is_dev || is_fisq
+             ?'FISQ: Festival International de Salsa de Quebec'
+             :'QSW: Quebec sensual weekend',
+
   SITE_EMAIL:'salsa_attitude@hotmail.com',
+
   MAX_FILE_SIZE:5*1024*1024,
   
-  API:process.env.NODE_ENV?.startsWith('dev')
-      ?'https://dev.arsou.com/api'
-      :'http://festivalsalsaquebec.com/api',
+  API:  is_dev?'https://dev.arsou.com/api'
+              :`http://${site}.com/api`,
 
-  IMG_URL:process.env.NODE_ENV?.startsWith('dev')
-      ?'https://dev.arsou.com/data/imgs'
-      :'http://festivalsalsaquebec.com/data/imgs'
+  IMG_URL: is_dev?'https://dev.arsou.com/data/imgs'
+                 :`http://${site}/data/imgs`
 }
